1caec9dbaf63ad018371fabaf15accae7863b964
KulikovVadim <pritinovin@gmail.com>
2021-10-01 23:28:37 +0300

    idzad1.1

--- 1.py
+++ 1.py
@@ -1,15 +1,20 @@
 d = 0
 try:
-	x = int(input("Введите целое число: "))
-	b = x
-	while b>0 :
-	    a = x % 10
-	    b = x // 10
-	    d = d * 10 + a
-	    x = b
-	print(d)
+    x = int(input("Введите целое число: "))
+    stok = x
+    x = abs(x)
+    b = x
+    while b>0 :
+        a = x % 10
+        b = x // 10
+        d = d * 10 + a
+        x = b
+    if stok > 0:
+        print(d)
+    else:
+        print (d * -1)
 except ValueError as message :
-	print ("Вы делаете что-то не то, попробуйте ввести целое число")
+    print ("Вы делаете что-то не то, попробуйте ввести целое число")
     
 
     
